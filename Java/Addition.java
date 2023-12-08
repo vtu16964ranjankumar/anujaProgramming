fd7df8c08ec9e4ed4d9425ea0d8c65c348a75833
Anuja Kumari <anujakumari8873@gmail.com>
2023-12-08 14:19:40 +0530

    Addition.java

---
+++ Java/Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+        int c = a + b;
+        System.out.println("The sum of a and b is: " + c);
+    }
+}
