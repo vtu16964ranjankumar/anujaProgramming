ba2ecf4655232a0fbb9f5a5ad7d26a5803b84ba1
Anuja Kumari <anujakumari8873@gmail.com>
2023-12-11 23:15:33 +0530

    Question Solved

---
+++ Java/alphabets.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class alphabets {
+    public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    char ch = sc.next().charAt(0);
+
+    for(ch='a'; ch<='z'; ch++){
+        System.out.print(ch+",");
+    }
+    
+    }
+}
+
---
+++ Java/evenNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class evenNumber {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(" Even Number is:"+n);
+
+        for( int i=1; i<=n;i++){
+          int c =2*i;
+
+            System.out.println(c);
+        }
+
+
+    }
+
+    
+}
